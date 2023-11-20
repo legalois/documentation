4dc8be86db8236a0de92fe50f6cebd324a7f0002
imbjdd <b0bb1e@protonmail.com>
2023-11-20 20:04:35 +0000

    fix: edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,14 +48,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/legalois/documentation',
+            'https://github.com/legalois/documentation/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/legalois/documentation',
+            'https://github.com/legalois/documentation/tree/main',
         },
         theme: {
           customCss: './src/css/custom.css',
